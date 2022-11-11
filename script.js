9a6212005fcaf0545955eab41bab73b8e95b6804
Nicolas Herbreteau <nicolasherbreteau@Nicolass-MacBook-Pro.local>
2022-11-11 15:24:50 +0000

    added emoji reaction on thank you card

--- script.js
+++ script.js
@@ -2,13 +2,35 @@ const form = document.getElementById('ratingForm')
 const ratingText = document.getElementById('ratingDisplay')
 const ratingCard = document.querySelector("#ratingCard")
 const thankYouCard = document.querySelector ('#thankYouCard')
+const smileyRating5 = document.querySelector('#smileyRating5')
+const smileyRating4 = document.querySelector('#smileyRating4')
+const smileyRating3 = document.querySelector('#smileyRating3')
+const smileyRating2 = document.querySelector('#smileyRating2')
+const smileyRating1 = document.querySelector('#smileyRating1')
+
+
 
 function rating () {
     let score = document.querySelector("input[type = 'radio']:checked").value
     ratingText.textContent = `You selected ${score} out of 5!`;
     ratingCard.style.display = 'none'
     thankYouCard.style.display = 'flex'
-
+    switch(score) {
+        case '5':
+            smileyRating5.style.display = 'flex'
+            break
+        case '4':
+            smileyRating4.style.display = 'flex'
+            break
+        case '3':
+            smileyRating3.style.display = 'flex'
+            break
+        case '2':
+            smileyRating2.style.display = 'flex'
+            break
+        default:
+            smileyRating1.style.display = 'flex'
+    }
 }
 
 form.addEventListener("submit", function(event) {
